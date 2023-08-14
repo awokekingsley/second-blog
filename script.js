15622393f9122d134d4f58436d071209737217fd
kingsley Awoke <kingsley_business@proton.me>
2023-08-14 08:38:02 +0100

    Added a function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-for (i = 0;i == 10; i++){
+for (i = 0;i < 10; i++){
     console.log(i);
 }
 
 const fruits = ["Apple", "Orange", "Pineapple"]
 
 console.log(fruits[0]);
+
+function myfunc(Boys, Girls) {
+    if(Girls > Boys) {
+         console.log("Girls are more populated in this world!");
+     } else {
+        console.log("Boys are more populated");
+        }
+
+myfunc(1333233338, 23322233);
+
