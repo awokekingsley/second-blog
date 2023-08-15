10761f4535b1dbba3aa9efff9ab4a88a365e2b0c
kingsley Awoke <kingsley_business@proton.me>
2023-08-15 11:07:12 +0100

    makes some changes, and add a while loop iterion examples.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// USING FOR LOOP TO LOOP INTO ITERATION
 for (i = 0;i < 10; i++){
     console.log(i);
 }
 
+// PRINTING AN ITEM INSIDE AN OBJECT
 const fruits = ["Apple", "Orange", "Pineapple"]
 
 console.log(fruits[0]);
 
+// FUNCTION FOR COMPARING BOYS ANF GIRLS NUMBER
 function myfunc(Boys, Girls) {
     if(Girls > Boys) {
          return "Girls are more populated in this world!"
@@ -18,3 +21,30 @@ let Boys = 100;
 let Girls = 300;
 
 console.log(myfunc(Boys,Girls));
+
+// NULLISH COALECING OPRATOR
+
+let user;
+
+console.log(user ?? "User not found");
+
+//THIS OPRATOR RETURNS DEFAULT VALUE, AS THE USER IS UNDERFINED TGEN IT RETUNRED THE DEFAULT VALUE WHICH IS THE STRING
+
+
+//WHILE LOOP AND DO...WHILE
+
+let x = 10;
+do {
+    console.log(x);
+    x++;
+}
+while (x < 100);
+
+//WHILE LOOP
+
+let y = 0;
+
+while (y < 20) {
+    console.log(y);
+    y++;
+}
